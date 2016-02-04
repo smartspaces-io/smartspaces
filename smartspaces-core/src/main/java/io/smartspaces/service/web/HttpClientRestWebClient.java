e10ef2b1161865815ad81ccecadfc8423403c1c3
Keith M. Hughes <keith@donbyas.hideho.org>
2016-02-04 15:28:08 -0700

    Add REST client

---
+++ smartspaces-core/src/main/java/io/smartspaces/service/web/HttpClientRestWebClient.java
@@ -0,0 +1,314 @@
+/*
+ * Copyright (C) 2014 Keith M. Hughes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.smartspaces.service.web;
+
+import io.smartspaces.SimpleSmartSpacesException;
+import io.smartspaces.SmartSpacesException;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Closeables;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.util.EntityUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+
+/**
+ * An {@link RestWebClient} which uses Apache HttpClient.
+ *
+ * @author Keith M. Hughes
+ */
+public class HttpClientRestWebClient implements RestWebClient {
+
+  /**
+   * The default number of total connections.
+   */
+  public static final int TOTAL_CONNECTIONS_ALLOWED_DEFAULT = 20;
+
+  /**
+   * Number of bytes in the copy buffer.
+   */
+  private static final int BUFFER_SIZE = 4096;
+
+  /**
+   * The HTTPClient instance which does the actual transfer.
+   */
+  private HttpClient httpClient;
+
+  /**
+   * Connection manager for the client.
+   */
+  private ThreadSafeClientConnManager httpConnectionManager;
+
+  /**
+   * The total number of connections allowed.
+   */
+  private final int totalConnectionsAllowed;
+
+  /**
+   * Construct a performer which allows a maximum of
+   * {@link #TOTAL_CONNECTIONS_ALLOWED_DEFAULT} connections.
+   */
+  public HttpClientRestWebClient() {
+    this(TOTAL_CONNECTIONS_ALLOWED_DEFAULT);
+  }
+
+  /**
+   * Construct a performer with a specified maximum number of connections.
+   *
+   * @param totalConnectionsAllowed
+   *          the maximum total number of connections allowed
+   */
+  public HttpClientRestWebClient(int totalConnectionsAllowed) {
+    this.totalConnectionsAllowed = totalConnectionsAllowed;
+  }
+
+  @Override
+  public void startup() {
+    httpConnectionManager = new ThreadSafeClientConnManager();
+    httpConnectionManager.setDefaultMaxPerRoute(totalConnectionsAllowed);
+    httpConnectionManager.setMaxTotal(totalConnectionsAllowed);
+
+    httpClient = new DefaultHttpClient(httpConnectionManager);
+  }
+
+  @Override
+  public void shutdown() {
+    if (httpConnectionManager != null) {
+      httpConnectionManager.shutdown();
+
+      httpConnectionManager = null;
+      httpClient = null;
+    }
+  }
+
+  @Override
+  public String performGet(String sourceUri) throws SmartSpacesException {
+    return performGet(sourceUri, Charsets.UTF_8);
+  }
+
+  @Override
+  public String performGet(String sourceUri, Charset charset) throws SmartSpacesException {
+    StringRestPerformer performer = new StringRestPerformer(charset);
+
+    HttpUriRequest request = new HttpGet(sourceUri);
+
+    performer.performRequest(request);
+
+    return performer.getResponse();
+  }
+
+  @Override
+  public String performPut(String sourceUri, String putContent) throws SmartSpacesException {
+    return performPut(sourceUri, putContent, Charsets.UTF_8);
+  }
+
+  @Override
+  public String performPut(String sourceUri, String putContent, Charset charset)
+      throws SmartSpacesException {
+    StringRestPerformer performer = new StringRestPerformer(charset);
+
+    try {
+      HttpPut request = new HttpPut(sourceUri);
+      request.setEntity(new StringEntity(putContent, charset.name()));
+
+      performer.performRequest(request);
+
+      return performer.getResponse();
+    } catch (UnsupportedEncodingException e) {
+      throw new SimpleSmartSpacesException(String.format(
+          "REST call to %s failed. Character set %s not supported", sourceUri, charset.name()));
+    }
+  }
+
+  @Override
+  public int getTotalConnectionsAllowed() {
+    return totalConnectionsAllowed;
+  }
+
+  /**
+   * A REST performer that gets String output.
+   *
+   * @author Keith M. Hughes
+   */
+  private class StringRestPerformer extends RestPerformer {
+
+    /**
+     * Output stream for the performer.
+     */
+    private ByteArrayOutputStream outputStream;
+
+    /**
+     * The charset for the bytes being read.
+     */
+    private Charset charset;
+
+    /**
+     * Construct a string performer.
+     *
+     * @param charset
+     *          the charset the string is expected in
+     */
+    public StringRestPerformer(Charset charset) {
+      this.charset = charset;
+    }
+
+    /**
+     * Get the string from the result in the required charset.
+     *
+     * @return the content of the result
+     */
+    public String getResponse() {
+      return new String(outputStream.toByteArray(), charset);
+    }
+
+    @Override
+    protected OutputStream getOutputStream() throws IOException {
+      outputStream = new ByteArrayOutputStream();
+
+      return outputStream;
+    }
+
+    @Override
+    protected String getDestinationDescription() {
+      return "string result";
+    }
+  }
+
+  /**
+   * The performer for HTTP responses. Subclasses decide the ultimate
+   * destination.
+   *
+   * @author Keith M. Hughes
+   */
+  private abstract class RestPerformer {
+
+    /**
+     * Create the output stream needed for the performer.
+     *
+     * @return the output steam to write to
+     *
+     * @throws IOException
+     *           an exception happened when obtaining the stream
+     */
+    protected abstract OutputStream getOutputStream() throws IOException;
+
+    /**
+     * Get a description of the destination for error reporting.
+     *
+     * @return a description of the destination for error reporting
+     */
+    protected abstract String getDestinationDescription();
+
+    /**
+     * Get the remote content from the source URI using an HTTP GET.
+     *
+     * @param sourceUri
+     *          the URI for the source content
+     */
+    public void performRequest(HttpUriRequest request) {
+      HttpEntity entity = null;
+      try {
+        HttpResponse response = httpClient.execute(request);
+
+        entity = response.getEntity();
+
+        int statusCode = response.getStatusLine().getStatusCode();
+        if (statusCode == HttpStatus.SC_OK) {
+          if (entity != null) {
+            InputStream in = entity.getContent();
+            try {
+              transferFile(in);
+
+              in.close();
+              in = null;
+            } catch (IOException e) {
+              throw new SmartSpacesException(
+                  String.format("Exception during REST call %s", request.getURI()), e);
+            } finally {
+              Closeables.closeQuietly(in);
+            }
+          }
+        } else {
+          throw new SimpleSmartSpacesException(
+              String.format("Server returned bad status code %d for REST call to URI %s",
+                  statusCode, request.getURI()));
+        }
+      } catch (SmartSpacesException e) {
+        throw e;
+      } catch (Exception e) {
+        throw new SmartSpacesException(
+            String.format("Could not read REST URI %s", request.getURI()), e);
+      } finally {
+        if (entity != null) {
+          try {
+            EntityUtils.consume(entity);
+          } catch (IOException e) {
+            throw new SmartSpacesException(String
+                .format("Could not consume entity content for REST call %s", request.getURI()), e);
+          }
+        }
+      }
+    }
+
+    /**
+     * Transfer the content from the HTTP input stream to the destination file.
+     *
+     * @param in
+     *          the HTTP result
+     *
+     * @throws IOException
+     *           something bad happened during IO operations
+     */
+    private void transferFile(InputStream in) throws IOException {
+      OutputStream out = null;
+      try {
+        out = getOutputStream();
+
+        byte[] buffer = new byte[BUFFER_SIZE];
+
+        int len;
+        while ((len = in.read(buffer)) > 0) {
+          out.write(buffer, 0, len);
+        }
+
+        out.flush();
+
+        out.close();
+        out = null;
+      } finally {
+        if (out != null) {
+          out.close();
+        }
+      }
+    }
+
+  }
+}
---
+++ smartspaces-core/src/main/java/io/smartspaces/service/web/RestWebClient.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2014 Keith M. Hughes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.smartspaces.service.web;
+
+import io.smartspaces.SmartSpacesException;
+import io.smartspaces.util.resource.ManagedResource;
+
+import java.nio.charset.Charset;
+
+/**
+ * Copy content from an HTTP URL to a file.
+ *
+ * <p>
+ * It is safe to have multiple threads copying content.
+ *
+ * @author Keith M. Hughes
+ */
+public interface RestWebClient extends ManagedResource {
+
+  /**
+   * See {@link #performGet(String, Charset)}.
+   *
+   * <p>
+   * The charset will be UTF-8.
+   *
+   * @param sourceUri
+   *          the URI to copy the content from
+   *
+   * @return the content
+   *
+   * @throws SmartSpacesException
+   *           if transfer was not successful
+   */
+  String performGet(String sourceUri) throws SmartSpacesException;
+
+  /**
+   * Get the content of the source URI and return as a string.
+   *
+   * <p>
+   * This method blocks until the transfer is complete or it fails.
+   *
+   * <p>
+   * This method will fail if there are not enough connections available and
+   * blocking until a connection becomes ready is not enabled.
+   *
+   * @param sourceUri
+   *          the URI to copy the content from
+   * @param charset
+   *          the charset the content will be in
+   *
+   * @return the content
+   *
+   * @throws SmartSpacesException
+   *           if transfer was not successful
+   */
+  String performGet(String sourceUri, Charset charset) throws SmartSpacesException;
+
+  /**
+   * See {@link #performGet(String, Charset)}.
+   *
+   * <p>
+   * The charset will be UTF-8.
+   *
+   * @param sourceUri
+   *          the URI to copy the content from
+   *
+   * @return the content
+   *
+   * @throws SmartSpacesException
+   *           if transfer was not successful
+   */
+  String performPut(String sourceUri, String putContent) throws SmartSpacesException;
+
+  /**
+   * Get the content of the source URI and return as a string.
+   *
+   * <p>
+   * This method blocks until the transfer is complete or it fails.
+   *
+   * <p>
+   * This method will fail if there are not enough connections available and
+   * blocking until a connection becomes ready is not enabled.
+   *
+   * @param sourceUri
+   *          the URI to copy the content from
+   * @param putContent
+   *          the content to put to the remote
+   * @param charset
+   *          the charset the content will be in
+   * 
+   * @return the content
+   *
+   * @throws SmartSpacesException
+   *           if transfer was not successful
+   */
+  String performPut(String sourceUri, String putContent, Charset charset)
+      throws SmartSpacesException;
+
+  /**
+   * Get the total number of simultaneous connections allowed.
+   *
+   * @return the total number of connections, or {@code 0} if there is no limit
+   */
+  int getTotalConnectionsAllowed();
+}
